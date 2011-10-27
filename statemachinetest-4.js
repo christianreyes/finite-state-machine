b1f2dad0c02b8e0df1542c11aa02c4a3ad31d031
Christian Reyes <cjreyes@andrew.cmu.edu>
2011-10-26 20:24:43 -0400

    added  probabalistic fsm. implementing it next commit

--- statemachinetest-4.js
+++ statemachinetest-4.js
@@ -1,197 +1,20 @@
 // Sample test file for the state machine class
 // Implements a simple draggable div which changes color when pressed.
 
-var _folder_icon_definition = {
-	states: [
-	{
-		name: "start",
-		transitions: [
-			{
-				input: "mouseDown", 
-				action: record_down_location,
-				endState: "down"
-			},
-			{
-				input: "doubleClick", 
-				action: open_folder,
-				endState: "start"
-			}]
-	},
-	{
-		name: "down",
-		transitions: [
-			{
-				input: "mouseUp",
-				action: do_drop,
-				endState: "start"
-			},
-			{
-				input: "mouseMove",
-				action: move_item,
-				endState: "down"
-			},
-			{
-				input: "mouseOut",
-				action: move_out,
-				endState: "start"
-			}
-		]
-	}
-	]
-};
-
-var _draw_icon_definition = {
-	states: [
-	{
-		name: "start",
-		transitions: [
-			{
-				input: "mouseDown", 
-				action: record_down_location,
-				endState: "down"
-			},
-			{
-				input: "doubleClick", 
-				action: open_canvas,
-				endState: "start"
-			}]
-	},
-	{
-		name: "down",
-		transitions: [
-			{
-				input: "mouseUp",
-				action: do_drop,
-				endState: "start"
-			},
-			{
-				input: "mouseMove",
-				action: move_item,
-				endState: "down"
-			},
-			{
-				input: "mouseOut",
-				action: move_out,
-				endState: "start"
-			}
-		]
-	}
-	]
-};
-
-var _folder_definition = {
-	states: [
-	{
-		name: "start",
-		transitions: [
-			{
-				input: "mouseDown", 
-				action: record_down_location,
-				endState: "down"
-			},
-			{
-				input: "mouseIn",
-				action: highlight,
-				endState: "start"
-			},
-			{
-				input: "mouseUp",
-				action: prompt,
-				endState: "start"
-			}]
-	},
-	{
-		name: "down",
-		transitions: [
-			
-			{
-				input: "mouseUp",
-				action: do_drop,
-				endState: "start"
-			},
-			{
-				input: "mouseMove",
-				action: move_item,
-				endState: "down"
-			},
-			{
-				input: "mouseOut",
-				action: move_out,
-				endState: "start"
-			}
-		]
-	}
-	]
-};
-
-// Record the location where the div was clicked.
-function record_down_location(e, attachedElement) {
-	log("record down");
-    attachedElement.downX = e.clientX;
-    attachedElement.downY = e.clientY;
-	
-	position = $(attachedElement).position();
-    attachedElement.origLeft = position.left;
-    attachedElement.origTop = position.top;
-	$(attachedElement).toggleClass("hover_border");
-	$(attachedElement).toggleClass("dragging");
+function poll_activity_low(e, attachedElement){
+	poll_activity(e, attachedElement, "low");
 }
 
-// When the div is released, make its background color red again.
-function letGo(attachedElement) {
-	log("let go");
-	$(attachedElement).toggleClass("hover_border");
-  	$(attachedElement).toggleClass("dragging");
+function poll_activity_medium(e, attachedElement){
+	poll_activity(e, attachedElement, "medium")
 }
 
-// Log that the div was dropped and change color
-function do_drop(e, attachedElement) {
-	log("do_drop");
-    letGo(attachedElement);
+function poll_activity_high(e, attachedElement){
+	poll_activity(e, attachedElement, "high")
 }
 
-function highlight(e, attachedElement) {
-	log(e);
-}
-
-function prompt(e,attachedElement){
-	log("just draggeed into");
-}
-
-// When mouse moves outside of region, log this.
-function move_out(e, attachedElement) {
-	log("move_out");
-    letGo(attachedElement);
-}
-
-// Moves the icon when the mouse moves.
-function move_item(e, attachedElement) {
-	attachedElement.style.left = (attachedElement.origLeft + (e.clientX - attachedElement.downX)) + "px";
-    attachedElement.style.top = (attachedElement.origTop + (e.clientY - attachedElement.downY)) + "px";
-}
-
-// 
-function open_folder(e, attachedElement) {
-	log("open_folder");
-	
-	var folder = document.createElement("div");
-	folder.id = "folder";
-	$(folder).addClass("folder");
-	
-	$('body').append(folder);
-	new StateMachine(_folder_definition, folder);
-}
-
-function open_canvas(e, attachedElement) {
-	log("open_canvas");
-	
-	var folder = document.createElement("canvas");
-	folder.id = "canvas";
-	$(folder).addClass("canvas");
-	$(folder).addClass("hover_border");
-	
-	$('body').append(folder);
-	new StateMachine(_folder_definition, folder);
+function poll_activity(e, attachedElement, level){
+	log("poll activity: " + level);
 }
 
 function log(message){
@@ -203,11 +26,61 @@ function log(message){
 
 // Provides the state machine description and creates a new state machine attached to myDiv
 window.onload = function() {
-	var folder_icon = document.getElementById("icon1");
-	var draw_icon = document.getElementById("icon2");
-   
-	//var smDoubleClick = new StateMachine(open_folder_definition, folder);
-	new StateMachine(_folder_icon_definition, folder_icon);
-	new StateMachine(_draw_icon_definition, draw_icon);
+	var activity = document.getElementById("activity");
 	
+	var activity_definition = {
+		states: [
+		{
+			name: "start",
+			transitions: [
+				{
+					input: "timerTick1000Ms", 
+					action: poll_activity_low,
+					endState: "low",
+					probability: .5
+				},
+				{
+					input: "timerTick1000Ms", 
+					action: poll_activity_medium,
+					endState: "medium",
+					probability: .25
+				},
+				{
+					input: "timerTick1000Ms", 
+					action: poll_activity_high,
+					endState: "high",
+					probability: .25
+				}]
+		},
+		{
+			name: "low",
+			transitions: [
+				{
+					input: "timerTick1000Ms", 
+					action: poll_activity_low,
+					endState: "start"
+				}]
+		},
+		{
+			name: "medium",
+			transitions: [
+				{
+					input: "timerTick1000Ms", 
+					action: poll_activity_medium,
+					endState: "start"
+				}]
+		},
+		{
+			name: "high",
+			transitions: [
+				{
+					input: "timerTick1000Ms", 
+					action: poll_activity_high,
+					endState: "start"
+				}]
+		}
+		]
+	};
+   
+	new StateMachine(activity_definition, activity);
 };
